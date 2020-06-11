9da14bfa4c1e02f360b22ea3bad0454ba3e9304f
Usuario <Usuario@JOHNDAY>
2020-06-11 11:33:44 -0500

    Agregar archivos de proyecto.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbCarrera = new System.Windows.Forms.ComboBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.txtCreditos = new System.Windows.Forms.TextBox();
+            this.txtAsignatura = new System.Windows.Forms.TextBox();
+            this.txtcodigo = new System.Windows.Forms.TextBox();
+            this.gridAsignatura = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gridAsignatura)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Código";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(154, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre de Asignatura";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(378, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Creditos";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(530, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Carrera";
+            // 
+            // cmbCarrera
+            // 
+            this.cmbCarrera.FormattingEnabled = true;
+            this.cmbCarrera.Items.AddRange(new object[] {
+            "Sistemas ",
+            "Tecnología de la Información",
+            "Contabilidad y  Auditoría ",
+            "Negocios Internacionales",
+            "Enfermería"});
+            this.cmbCarrera.Location = new System.Drawing.Point(479, 64);
+            this.cmbCarrera.Name = "cmbCarrera";
+            this.cmbCarrera.Size = new System.Drawing.Size(227, 24);
+            this.cmbCarrera.TabIndex = 4;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(712, 58);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(85, 38);
+            this.btnAgregar.TabIndex = 5;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // txtCreditos
+            // 
+            this.txtCreditos.Location = new System.Drawing.Point(370, 66);
+            this.txtCreditos.MaxLength = 2;
+            this.txtCreditos.Name = "txtCreditos";
+            this.txtCreditos.Size = new System.Drawing.Size(84, 22);
+            this.txtCreditos.TabIndex = 6;
+            // 
+            // txtAsignatura
+            // 
+            this.txtAsignatura.Location = new System.Drawing.Point(129, 69);
+            this.txtAsignatura.MaxLength = 30;
+            this.txtAsignatura.Name = "txtAsignatura";
+            this.txtAsignatura.Size = new System.Drawing.Size(208, 22);
+            this.txtAsignatura.TabIndex = 7;
+            // 
+            // txtcodigo
+            // 
+            this.txtcodigo.Location = new System.Drawing.Point(12, 69);
+            this.txtcodigo.MaxLength = 10;
+            this.txtcodigo.Name = "txtcodigo";
+            this.txtcodigo.Size = new System.Drawing.Size(101, 22);
+            this.txtcodigo.TabIndex = 8;
+            // 
+            // gridAsignatura
+            // 
+            this.gridAsignatura.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridAsignatura.Location = new System.Drawing.Point(12, 144);
+            this.gridAsignatura.Name = "gridAsignatura";
+            this.gridAsignatura.RowHeadersWidth = 51;
+            this.gridAsignatura.RowTemplate.Height = 24;
+            this.gridAsignatura.Size = new System.Drawing.Size(759, 294);
+            this.gridAsignatura.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 124);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(157, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Listado de Asignaturas ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.gridAsignatura);
+            this.Controls.Add(this.txtcodigo);
+            this.Controls.Add(this.txtAsignatura);
+            this.Controls.Add(this.txtCreditos);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.cmbCarrera);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Lista Génericas";
+            ((System.ComponentModel.ISupportInitialize)(this.gridAsignatura)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbCarrera;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox txtCreditos;
+        private System.Windows.Forms.TextBox txtAsignatura;
+        private System.Windows.Forms.TextBox txtcodigo;
+        private System.Windows.Forms.DataGridView gridAsignatura;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApp1/modelo/Asignatura.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1.modelo
+{
+   public class Asignatura
+    {
+        public string codigo { get; set; }
+      public string asignatura { get; set; }
+        public int creditos { get; set; }
+        public string carrera { get; set; }
+    }
+}
